c04538dab61cba69d3f63d430f7dc4d70aacab9c
Jeffbezus <jefersonguedesdeoliveira@gmail.com>
2023-06-20 19:49:43 -0300

    Board exception and defensive programming

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -6,6 +6,9 @@ public class Board {
     private Piece[][] pieces;
 
     public Board(int rows, int columns){
+        if(rows < 1 || columns < 1){
+            throw new boardException("Error creating board: there must be at least 1 row and 1 column");
+        }
         this.rows = rows;
         this.columns = columns;
         pieces = new Piece[rows][columns];
@@ -13,44 +16,52 @@ public class Board {
     }
     
     public Piece piece(int row, int column){
+        if (!positionExists(row, column)){
+            throw new boardException("Position not on the board");
+        }
         return pieces[row][column];
     }
     
     public Piece piece(Position position){
+        if (!positionExists(position)){
+            throw new boardException("Position not on the board");
+        }
         return pieces[position.getRow()][position.getColunm()];
     }
 
     public void placePiece(Piece piece, Position position){
+        if(thereIsAPiece(position)){
+            throw new boardException("There is already a piece on position" + position);
+        }
         pieces[position.getRow()][position.getColunm()] = piece;
-
+        piece.position = position;
     }
 
     public Piece removePiece(Position position){
         return null;
     }
 
-    public Boolean positionExists(Position position){
-        return null;
+    private boolean positionExists(int row, int column){
+        return row >= 0 && row < rows && column >= 0 && column < columns;
+    }
+
+    public boolean positionExists(Position position){
+        return positionExists(position.getRow(), position.getColunm());
     }
 
     public Boolean thereIsAPiece(Position position){
-        return null;
+        if (!positionExists(position)){
+            throw new boardException("Position not on the board");
+        }
+        return piece(position) != null;
     }
 
     public int getRows() {
         return rows;
     }
 
-    public void setRows(int rows) {
-        this.rows = rows;
-    }
-
-    public int getColumns() {
+   public int getColumns() {
         return columns;
-    }
-
-    public void setColumns(int columns) {
-        this.columns = columns;
     }
 
 }
---
+++ src/boardgame/boardException.java
@@ -0,0 +1,9 @@
+package boardgame;
+
+public class boardException extends RuntimeException{
+    
+    public boardException(String msg){
+        super(msg);
+    }
+
+}
