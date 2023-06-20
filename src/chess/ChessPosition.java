e83a89adc6d77036cf26776149dfe2c3651c1b65
Jeffbezus <jefersonguedesdeoliveira@gmail.com>
2023-06-20 20:52:32 -0300

    ChessException and ChessPosition

--- src/chess/ChessPosition.java
+++ src/chess/ChessPosition.java
@@ -3,12 +3,35 @@ package chess;
 import boardgame.Position;
 
 public class ChessPosition {
+    private char column;
+    private int row;
 
-    protected Position toPosition(){
-        return null;
+    public ChessPosition(char column, int row) {
+        if (column < 'a' || column > 'h'|| row < 1 || row > 8){
+            throw new chessException("Error instantianting ChessPosition. Valid values are from a1 to h8.");
+        }
+        this.column = column;
+        this.row = row;
     }
 
-    protected ChessPosition fromPosition(Position position){
-        return null;
+    protected Position toPosition(){
+        return new Position(8 - row, column - 'a');
+    }
+
+    protected static ChessPosition fromPosition(Position position){ 
+        return new ChessPosition((char)('a' - position.getColunm()), 8 - position.getRow());
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+
+    @Override
+    public String toString() {
+        return " " + column +  row;
     }
 }
